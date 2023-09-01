aa00d90f0cdaf523782c07a8af1c8c6cbe6016b3
bluezoopla <59399623+Undraoid@users.noreply.github.com>
2023-09-01 05:15:57 +0100

    come on

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,114 @@
-import Image from 'next/image'
+"use client";
+import { useState } from 'react';
+
+const currencyCodes: { [key: string]: string } = {
+  USD: 'en-US',
+  GBP: 'en-GB',
+  EUR: 'en-EU',
+  // Add more currency codes as needed
+};
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [productCost, setProductCost] = useState<number>(0);
+  const [sellPrice, setSellPrice] = useState<number>(0);
+  const [quantity, setQuantity] = useState<number>(0);
+  const [currency, setCurrency] = useState<string>('USD');
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const handleCheck = () => {
+    const profit = sellPrice - productCost;
+    const revenue = sellPrice * quantity - productCost * quantity;
+
+    const formattedProfit = formatCurrency(profit);
+    const formattedRevenue = formatCurrency(revenue);
+
+    setResult({
+      profit: formattedProfit,
+      revenue: formattedRevenue,
+    });
+  };
+
+  const formatCurrency = (value: number): string => {
+    const languageCode = currencyCodes[currency] || 'en-US';
+    return new Intl.NumberFormat(languageCode, {
+      style: 'currency',
+      currency: currency,
+    }).format(value);
+  };
+
+  const [result, setResult] = useState<{ profit: string; revenue: string }>({
+    profit: '',
+    revenue: '',
+  });
+
+  return (
+    <div className="flex flex-col space-y-4 justify-center items-center">
+      <div>
+        <label htmlFor="productCost" className="block text-gray-700 font-semibold">
+          Product Cost
+        </label>
+        <input
+          type="number"
+          id="productCost"
+          name="productCost"
+          value={productCost}
+          onChange={(e) => setProductCost(Number(e.target.value))}
+          className="w-full px-4 py-2 mt-1 border rounded-lg focus:ring focus:ring-blue-200"
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        <label htmlFor="sellPrice" className="block text-gray-700 font-semibold">
+          Sell Price
+        </label>
+        <input
+          type="number"
+          id="sellPrice"
+          name="sellPrice"
+          value={sellPrice}
+          onChange={(e) => setSellPrice(Number(e.target.value))}
+          className="w-full px-4 py-2 mt-1 border rounded-lg focus:ring focus:ring-blue-200"
+        />
       </div>
-    </main>
-  )
+      <div>
+        <label htmlFor="quantity" className="block text-gray-700 font-semibold">
+          Quantity
+        </label>
+        <input
+          type="number"
+          id="quantity"
+          name="quantity"
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+          className="w-full px-4 py-2 mt-1 border rounded-lg focus:ring focus:ring-blue-200"
+        />
+      </div>
+      <div>
+        <label htmlFor="currency" className="block text-gray-700 font-semibold">
+          Currency
+        </label>
+        <select
+          id="currency"
+          name="currency"
+          value={currency}
+          onChange={(e) => setCurrency(e.target.value)}
+          className="w-full px-4 py-2 mt-1 border rounded-lg focus:ring focus:ring-blue-200"
+        >
+          <option value="USD">USD</option>
+          <option value="GBP">GBP</option>
+          <option value="EUR">EUR</option>
+          {/* Add more currency options as needed */}
+        </select>
+      </div>
+      <button
+        type="button"
+        onClick={handleCheck}
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      >
+        Check
+      </button>
+      <div className="text-3xl font-sans font-bold space-y-2 bg-gray-100 shadow-md p-4 rounded-md">
+        <h2>Profit: {result.profit}</h2>
+        <h2>Revenue: {result.revenue}</h2>
+      </div>
+    </div>
+  );
 }
