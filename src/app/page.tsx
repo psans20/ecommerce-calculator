b769e8f9324f4e499cb57c087a419fbb658a1484
bluezoopla <59399623+Undraoid@users.noreply.github.com>
2023-09-07 21:48:02 +0100

    investment feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,25 +9,32 @@ const currencyCodes: { [key: string]: string } = {
 };
 
 export default function Home() {
-  const [productCost, setProductCost] = useState<number>(0.0); // Update initial value
-  const [sellPrice, setSellPrice] = useState<number>(0.0); // Update initial value
-  const [quantity, setQuantity] = useState<number>(0.0); // Update initial value
+  const [productCost, setProductCost] = useState<number>(0.0);
+  const [sellPrice, setSellPrice] = useState<number>(0.0);
+  const [quantity, setQuantity] = useState<number>(0.0);
+  const [investment, setInvestment] = useState<number>(0.0);
   const [currency, setCurrency] = useState<string>('USD');
+  const [activeTab, setActiveTab] = useState<string>('Sold'); // Initialize to 'Sold'
   const [cardVisible, setCardVisible] = useState<boolean>(false);
 
   const handleCheck = () => {
     const profit = sellPrice - productCost;
-    const revenue = sellPrice * quantity - productCost * quantity;
-    const total = productCost + sellPrice * quantity;
+        const sold = investment / productCost;
+            const revenue =  (activeTab === 'Sold' ? profit * quantity : profit * sold);
+    const total =  (activeTab === 'Sold' ? sellPrice * quantity : sellPrice * sold);
+    const invest = productCost * quantity;
 
     const formattedProfit = formatCurrency(profit);
     const formattedRevenue = formatCurrency(revenue);
     const formatTotal = formatCurrency(total);
+    const formatInvest = formatCurrency(invest);
 
     setResult({
       profit: formattedProfit,
       revenue: formattedRevenue,
       total: formatTotal,
+      invest: formatInvest,
+      sold: sold
     });
 
     setCardVisible(true);
@@ -41,10 +48,18 @@ export default function Home() {
     }).format(value);
   };
 
-  const [result, setResult] = useState<{ profit: string; revenue: string; total: string }>({
+  const [result, setResult] = useState<{
+    profit: string;
+    revenue: string;
+    total: string;
+    invest: string;
+    sold: string;
+  }>({
     profit: '',
     revenue: '',
     total: '',
+    invest: '',
+    sold: ''
   });
 
   return (
@@ -54,43 +69,60 @@ export default function Home() {
           Product Cost
         </label>
         <input
-    type="number"
-    step="0.01"
-    id="productCost"
-    name="productCost"
-    value={productCost}
-    onChange={(e) => setProductCost(parseFloat(e.target.value))}
-    className="w-full px-4 py-2 mt-1 border rounded-lg focus:ring focus:ring-blue-200"
-  />
-</div>
-<div>
-  <label htmlFor="sellPrice" className="block text-gray-700 font-semibold">
-    Sell Price
-  </label>
-  <input
-    type="number"
-    step="0.01"
-    id="sellPrice"
-    name="sellPrice"
-    value={sellPrice}
-    onChange={(e) => setSellPrice(parseFloat(e.target.value))}
-    className="w-full px-4 py-2 mt-1 border rounded-lg focus:ring focus:ring-blue-200"
-  />
-</div>
-<div>
-  <label htmlFor="quantity" className="block text-gray-700 font-semibold">
-    Quantity
-  </label>
-  <input
-    type="number"
-    step="0.01"
-    id="quantity"
-    name="quantity"
-    value={quantity}
-    onChange={(e) => setQuantity(parseFloat(e.target.value))}
-    className="w-full px-4 py-2 mt-1 border rounded-lg focus:ring focus:ring-blue-200"
-  />
+          type="number"
+          step="0.01"
+          id="productCost"
+          name="productCost"
+          value={productCost}
+          onChange={(e) => setProductCost(parseFloat(e.target.value))}
+          className="w-full px-4 py-2 mt-1 border rounded-lg focus:ring focus:ring-blue-200"
+        />
       </div>
+      <div>
+        <label htmlFor="sellPrice" className="block text-gray-700 font-semibold">
+          Sell Price
+        </label>
+        <input
+          type="number"
+          step="0.01"
+          id="sellPrice"
+          name="sellPrice"
+          value={sellPrice}
+          onChange={(e) => setSellPrice(parseFloat(e.target.value))}
+          className="w-full px-4 py-2 mt-1 border rounded-lg focus:ring focus:ring-blue-200"
+        />
+      </div>
+      {activeTab === 'Sold' ? ( // Conditionally render based on the active tab
+        <div>
+          <label htmlFor="quantity" className="block text-gray-700 font-semibold">
+            Quantity
+          </label>
+          <input
+            type="number"
+            step="0.01"
+            id="quantity"
+            name="quantity"
+            value={quantity}
+            onChange={(e) => setQuantity(parseFloat(e.target.value))}
+            className="w-full px-4 py-2 mt-1 border rounded-lg focus:ring focus:ring-blue-200"
+          />
+        </div>
+      ) : (
+        <div>
+          <label htmlFor="investment" className="block text-gray-700 font-semibold">
+            Investment
+          </label>
+          <input
+            type="number"
+            step="0.01"
+            id="investment"
+            name="investment"
+            value={investment}
+            onChange={(e) => setInvestment(parseFloat(e.target.value))}
+            className="w-full px-4 py-2 mt-1 border rounded-lg focus:ring focus:ring-blue-200"
+          />
+        </div>
+      )}
       <div>
         <label htmlFor="currency" className="block text-gray-700 font-semibold">
           Currency
@@ -108,6 +140,27 @@ export default function Home() {
           {/* Add more currency options as needed */}
         </select>
       </div>
+      <div>
+        <label className="block text-gray-700 font-semibold text-center p-4">Select a Tab</label>
+        <div className="flex space-x-4">
+          <button
+            onClick={() => setActiveTab('Sold')}
+            className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 ${
+              activeTab === 'Sold' ? 'bg-blue-700' : 'bg-gray-400'
+            } focus:outline-none`}
+          >
+            Sold
+          </button>
+          <button
+            onClick={() => setActiveTab('Invested')}
+            className={`text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 ${
+              activeTab === 'Invested' ? 'bg-green-700' : 'bg-gray-400'
+            } focus:outline-none`}
+          >
+            Invested
+          </button>
+        </div>
+      </div>
       <button
         type="button"
         onClick={handleCheck}
@@ -115,12 +168,23 @@ export default function Home() {
       >
         Check
       </button>
-      <div className={`text-3xl font-mono font-bold space-y-2 bg-zinc-800 text-gray-100 shadow-md p-4 m-4 rounded-md text-center ${cardVisible ? 'block' : 'hidden'}`} id='card'>
+      <div
+        className={`text-3xl font-mono font-bold space-y-2 bg-zinc-800 text-gray-100 shadow-md p-4 m-4 rounded-md text-center ${
+          cardVisible ? 'block' : 'hidden'
+        }`}
+        id="card"
+      >
+        {activeTab === 'Sold' && (
+          <h2>Investment: <span className='text-yellow-500'>{result.invest}</span></h2>
+        )}
+        {activeTab !== 'Sold' && (
+          <h2>Quantity: <span className='text-yellow-500'>{result.sold}</span></h2>
+        )}
+        
         <h2>Profit Margin: <span className='text-emerald-500'>{result.profit}</span></h2>
         <h2>Total Profit: <span className='text-emerald-500'>{result.revenue}</span></h2>
-        <h2>Revenue: <span className='text-emerald-500'>{result.total}</span></h2>
+        <h2>Revenue: <span className='text-teal-500'>{result.total}</span></h2>
       </div>
     </div>
   );
 }
-
